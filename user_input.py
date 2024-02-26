eca9616b96ea1311bcacc9f1d2ea3fc89966c129
isaacwach <isaacwachira57@gmail.com>
2024-02-26 07:20:16 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("what is your age? ")
+location = input("What is your location? ")
+
+print("Hello {}, yot are {} years old and live in {}.".format(name, age, location))
